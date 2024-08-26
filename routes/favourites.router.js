1fc2f83581d17ca2058ac2227863a144c3a3973e
sunheeday <k2mseungh2@gmail.com>
2024-08-26 21:35:22 +0900

    router

--- routes/favourites.router.js
+++ routes/favourites.router.js
@@ -3,6 +3,6 @@ const favouritesRouter = express.Router();
 const favouritesController = require("../controllers/favourites.controller");
 
 favouritesRouter.get("/get/:userId", favouritesController.getFavouritesClothes);
-favouritesRouter.post("/", favouritesController.postFavouritesClothes);
+favouritesRouter.post("/like", favouritesController.postFavouritesClothes);
 
 module.exports = favouritesRouter;
