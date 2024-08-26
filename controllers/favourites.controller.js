f8c816a05678f9fa63eaddf0189df949cac1250c
sunheeday <k2mseungh2@gmail.com>
2024-08-26 20:28:43 +0900

    postLikeClothes

--- controllers/favourites.controller.js
+++ controllers/favourites.controller.js
@@ -1,2 +1,16 @@
 // postLikeClothes(req, res): 옷 찜하기 처리
 // getFavouriteClothes(req, res): 사용자의 찜한 옷 가져오기
+
+const FavouritesModel = require('../models/favourites.model');
+
+async function postLikeClothes(req, res){
+  try{
+    const {} = req.body;
+    console.log(req.body);
+  }catch(error){
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
+module.exports = { postLikeClothes, }
